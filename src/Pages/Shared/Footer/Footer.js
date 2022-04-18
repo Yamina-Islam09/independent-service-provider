69729d2019f1efc2018189d91fc493d5cd200a41
Yamina-Islam09 <yamina151999@gmail.com>
2022-04-18 17:41:18 +0600

    footer portion done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-             <h2>Footer</h2>
-        </div>
-    );
+  return (
+    <div className="container-fluid bg-light">
+      <div className='d-flex align-items-center justify-content-center'>
+        <p>Acquolina Catering & Event Management</p>
+        <p>2291 2nd Avenue, New York, NY, 10035 212.426.9400</p>
+        <p> info@acquolinacatering.com</p>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
